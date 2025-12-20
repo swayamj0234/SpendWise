6c0b2b480edaf2f5acf8a563ab23b43ae29bcb83
swayamj0234 <jain.swayam0234@gmail.com>
2025-12-20 13:01:17 +0100

    Classes
    added classes expense and expense_tracker

---
+++ expense.py
@@ -0,0 +1,6 @@
+class Expense:
+    def __init__(self, name, amount, category):
+        self.name = name
+        self.amount = amount
+        self.category = category  
+        
---
+++ expense_tracker.py
@@ -0,0 +1,6 @@
+def main():
+    print("Welcome to the Expense Tracker!")
+    pass
+
+if __name__ == "__main__":  #only runs when this file is executed directly
+ main()
