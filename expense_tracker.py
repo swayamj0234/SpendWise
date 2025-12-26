689193c570d629ae15fd0fb2f85d6ea85039066f
swayamj0234 <jain.swayam0234@gmail.com>
2025-12-26 20:43:39 +0100

    Added Funtions

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,6 +1,21 @@
 def main():
-    print("Welcome to the Expense Tracker!")
-    pass
+    print("🚀Welcome to the Expense Tracker!")
+    
+    add_expense()
+
+    save_expenses_to_file()
+
+    summarize_expenses()
+
+
+def add_expense():   #function to add an expense
+    print(f"adding expense..")
+    
+def save_expenses_to_file():  #function to save expenses to a file
+    print(f"saving expenses to file..")
+
+def summarize_expenses():  #function to summarize expenses
+    print(f"summarizing expenses..")
 
 if __name__ == "__main__":  #only runs when this file is executed directly
- main()
+    main()
