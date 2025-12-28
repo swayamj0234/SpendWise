0ff2f4ff908913705f921df30f3c024f27c55e7d
swayamj0234 <jain.swayam0234@gmail.com>
2025-12-28 21:16:36 +0100

    Updated some Methods

--- expense_tracker.py
+++ expense_tracker.py
@@ -3,13 +3,41 @@ def main():
     
     add_expense()
 
-    save_expenses_to_file()
+    #save_expenses_to_file()
 
-    summarize_expenses()
+    #summarize_expenses()
 
 
 def add_expense():   #function to add an expense
     print(f"adding expense..")
+    expense_name = input("Enter expense name: ")
+    expense_amount = float(input("Enter expense amount: "))
+    print(f"You've entered : {expense_name}, {expense_amount: .2f} €")
+
+    expense_category = [
+        "Food 🍔",
+        "Transport 🚗",
+        "Entertainment 🎮",
+        "Home 🏠",
+        "Miscellaneous 🧾",
+    ]
+
+    while True:
+        print("Select expense category:")
+        for i, category in enumerate(expense_category, 1): #enumerate to number the categories
+            print(f"  {i}. {category}")
+
+        value_range = f"[1 - {len(expense_category)}]"
+        selected_category = int(input(f"Enter category number {value_range}: "))
+
+        if selected_category in range(1, len(expense_category) + 1):
+          break
+        else:
+            print(f"Invalid input. Please enter a number {value_range}.")
+    print(f"Category selected: {expense_category[selected_category - 1]}")  #-1 to adjust for 0-based index
+     
+    
+ 
     
 def save_expenses_to_file():  #function to save expenses to a file
     print(f"saving expenses to file..")
